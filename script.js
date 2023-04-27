c35fddf47e129347018afc6b1abaaed1d0d722a7
vvishalpatil <vishalpatil948@gmail.com>
2023-04-28 00:33:29 +0530

    completed intro section and contact section

---
+++ script.js
@@ -0,0 +1,9 @@
+function navigateTomail(){
+    window.open('mailto:vishalpatil948@gmail.com',"_self");
+}
+function navigateTodiler(){
+    window.open('tel:+918554907837',"_self");
+}
+function navigateToLinkedIn(){
+    window.open("https://linkedin.com/in/vishalpatil948");
+}
